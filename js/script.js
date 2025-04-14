67b45a00be4ee75874662047babfb5707a79c5d9
Eduardo Azevedo <eduardoazvd17@gmail.com>
2025-04-14 16:29:28 -0300

    refactor: remove testimonials section and update contact information

--- js/script.js
+++ js/script.js
@@ -1,40 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const testimonials = document.querySelectorAll('.testimonial');
-    const dots = document.querySelectorAll('.dot');
-    let currentTestimonial = 0;
-
-    hideAllTestimonials();
-    testimonials[0].style.display = 'block';
-    dots[0].classList.add('active');
-
-    function hideAllTestimonials() {
-        testimonials.forEach(testimonial => {
-            testimonial.style.display = 'none';
-        });
-
-        dots.forEach(dot => {
-            dot.classList.remove('active');
-        });
-    }
-
-    function showNextTestimonial() {
-        hideAllTestimonials();
-        currentTestimonial = (currentTestimonial + 1) % testimonials.length;
-        testimonials[currentTestimonial].style.display = 'block';
-        dots[currentTestimonial].classList.add('active');
-    }
-
-    setInterval(showNextTestimonial, 5000);
-
-    dots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-            hideAllTestimonials();
-            currentTestimonial = index;
-            testimonials[currentTestimonial].style.display = 'block';
-            dots[currentTestimonial].classList.add('active');
-        });
-    });
-
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
